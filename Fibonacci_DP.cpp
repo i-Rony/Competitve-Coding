3ac59a3f16eba35114aafb15d04dc9b8095d4a9d
i_Rony <ronrocks999@gmail.com>
2020-06-14 21:05:58 +0530

    Space Optimised DP Commit plus Bottom Up added equal operator in for loop

--- Fibonacci_DP.cpp
+++ Fibonacci_DP.cpp
@@ -24,10 +24,29 @@ int fib_TD(int n, int dp[]){
 int fib_BU(int n){
     int dp[1000] = {0};
     dp[1] = 1;
-    for(int i = 2; i < n; i++){
+    for(int i = 2; i <= n; i++){
         dp[i] = dp[i - 1] + dp[i - 2];
     }
     return dp[n];
+}
+
+// Bottom Up Space Optimised
+int fib_SpaceOpt(int n){
+    
+    if(n == 0 || n==1){
+        return n;
+    }
+
+    int a = 0;
+    int b = 1;
+    int c;
+
+    for(int i = 2; i <= n; i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 }
 
 int main(){
@@ -37,6 +56,7 @@ int main(){
 
     cout << fib_BU(n) << endl;
     cout << fib_TD(n, dp) << endl;
-    
+    cout << fib_SpaceOpt(n) << endl;
+
     return 0;
 }
