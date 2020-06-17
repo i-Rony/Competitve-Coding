161a67f0869961cb6c96c5a6b4aba7a529fe26a5
i_Rony <ronrocks999@gmail.com>
2020-06-17 17:08:55 +0530

    Graph Commit 2 plus corrections

--- AdjacencyList.cpp
+++ AdjacencyList.cpp
@@ -20,7 +20,7 @@ class Graph{
         }
         void printAdjList(){
             for(int i = 0; i < V; i++){
-                cout << "Vertex " << i << " ->";
+                cout << "Vertex " << i << " -> ";
                 for(int nbr: l[i]){
                     cout << nbr << ",";
                 }
@@ -36,5 +36,6 @@ int main(){
     g.addEdge(0,2);
     g.addEdge(2,3);
     g.addEdge(1,2);
+    g.printAdjList();
     return 0;
 }
